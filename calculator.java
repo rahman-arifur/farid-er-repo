28e5aeaca2423de879818241341084525cecea2f
tophhh <arifurr946@gmail.com>
2025-08-21 20:00:11 +0600

    feature: modify Calculator.divide message (VERSION_FEATURE)

--- calculator.java
+++ calculator.java
@@ -1,17 +1,16 @@
 public class Calculator {
-	
+	public int subtract(int a, int b) {
+        return a - b;
+    }
+
     public double divide(int a, int b) {
-        if (b == 0) throw new IllegalArgumentException("Division by zero");
+    	if (b == 0) throw new IllegalArgumentException("Division by zero - FEATURE");
         return (double) a / b;
     }
     public Calculator() { }
 
     public int add(int a, int b) {
         return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
     }
 
     public int multiply(int a, int b) {
